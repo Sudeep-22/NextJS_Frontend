3de734d1de16227c76f927c14b080bd8b74a90c7
SudeepPoojari <114012832+Sudeep-22@users.noreply.github.com>
2024-06-17 12:37:12 +0530

    Added the featured courses portions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import Hero_section from "@/components/Hero_section";
 import Navbar from "@/components/Navbar";
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Navbar/>
     <Hero_section/>
+    <FeaturedCourses/>
     </main>
 
   );

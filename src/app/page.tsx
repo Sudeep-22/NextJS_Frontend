346bf3f740ad3332b9bc07093f4cf1ed84a8ab21
SudeepPoojari <114012832+Sudeep-22@users.noreply.github.com>
2024-06-17 14:45:30 +0530

    Added the sticky scroll and infinite moving scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import Hero_section from "@/components/Hero_section";
 import Navbar from "@/components/Navbar";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Navbar/>
     <Hero_section/>
     <FeaturedCourses/>
+    <WhyChooseUs />
+    <TestimonialCards />
     </main>
 
   );

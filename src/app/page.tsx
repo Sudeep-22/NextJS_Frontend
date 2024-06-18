041211d058737cf41c8d036afb965a415e4fcd44
SudeepPoojari <114012832+Sudeep-22@users.noreply.github.com>
2024-06-18 20:58:11 +0530

    FrondEnd completed, some pages remaining

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import Hero_section from "@/components/Hero_section";
 import Instructors from "@/components/Instructors";
-import Navbar from "@/components/Navbar";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <Navbar/>
     <Hero_section/>
     <FeaturedCourses/>
     <WhyChooseUs />
     <TestimonialCards />
     <UpcomingWebinar />
     <Instructors />
+    <Footer />
     </main>
 
   );
